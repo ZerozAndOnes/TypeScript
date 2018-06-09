226b2bf007230481b0615a5343982aa99ab191e6
ZerozAndOnes <iemmudassir@gmail.com>
2018-06-09 11:56:51 +0300

    Make generic class generic :P

--- ClassExample/genericClass.ts
+++ ClassExample/genericClass.ts
@@ -1,8 +1,11 @@
-class Generic {
-    constructor() {}
-    myGenericFunction<TYPE>(someParameter: TYPE) {
-        return someParameter;
+class Generic<T1> {
+    constructor(public value1: T1) {}
+    myGenericFunction<T>(someParameter: T) {
+        // Cast the values to string 
+        // (requires a double cast - first to any and then to string) 
+        // and return
+        return <string><any>this.value1 + someParameter;
     }
 }
 
-console.log(new Generic().myGenericFunction<string>("Hello"));
+console.log(new Generic<number>(10).myGenericFunction<string>("Hello"));
