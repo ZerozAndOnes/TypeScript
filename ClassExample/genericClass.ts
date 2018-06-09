9b1b6f8c236e7cdfdc0acec9c6e2e0f3101f3059
ZerozAndOnes <iemmudassir@gmail.com>
2018-06-09 12:08:47 +0300

    Constrain generic type

--- ClassExample/genericClass.ts
+++ ClassExample/genericClass.ts
@@ -1,6 +1,9 @@
 class Generic<T1> {
+    
     constructor(public value1: T1) {}
-    myGenericFunction<T>(someParameter: T) {
+
+    // Define generic function with parameter constrained to be either a string or number
+    myGenericFunction<T extends string | number>(someParameter: T) {
         // Cast the values to string 
         // (requires a double cast - first to any and then to string) 
         // and return
