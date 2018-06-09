3085a486d1df096875092924d9ebc685ed08413c
ZerozAndOnes <iemmudassir@gmail.com>
2018-06-10 02:22:31 +0300

    Initialize fields using Access Modifier in Constructor Parameters.

--- ClassExample/classexample.ts
+++ ClassExample/classexample.ts
@@ -1,10 +1,14 @@
 class Greeter {
+    /***************** See Alternative
     greeting: string; 
     // A string property. Can also be defined in constructor as public message: string
     
     constructor(message: string) { // A constructor that takes a string parameter
         this.greeting = message; 
-    }
+    }*/
+
+    /***** Alternative by defining access modifier in Constructor Parameters */
+    constructor(private greeting: string) {}
 
     greet() {
         return "Hello, " + this.greeting;
