8babccb6f18fa97256ab4acbcb33b861028c8a45
ZerozAndOnes <iemmudassir@gmail.com>
2018-06-10 02:34:26 +0300

    Use camelCase for properties.

--- ClassExample/classProperties.ts
+++ ClassExample/classProperties.ts
@@ -1,16 +1,16 @@
 class Point {
-    constructor(private x: number, private y: number) {}
+    constructor(private _x: number, private _y: number) {}
 
     // We can implement properties as methods i.e., setX and getX
     // But that is not the preferred way, the preferred way is to use the get and set keywords i.e.,
-    get X() {
-        return this.x;
+    get x() {
+        return this._x;
     }
 
-    set X(value) {
+    set x(value) {
         if(value < 0 )
             throw new Error("Value can't be less than 0.");
-        this.x = value;
+        this._x = value;
     }
     
 }
